80fee3d7aef7b9c68273dee8c75a89b8bf5f0ed0
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2018-06-12 12:47:33 -0400

    Primeiro EndPoint criado para Product

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductsController extends Controller
+{
+    public function index() {
+        
+        return Product::all();   
+    }
+
+    public function store() {
+
+    }
+
+    public function update($id) {
+
+    }
+
+    public function delete($id) {
+        
+    }
+}
