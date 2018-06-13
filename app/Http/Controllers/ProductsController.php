bde7ca81a6ffb0afb3d8238cf8f1785dd0393ab9
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2018-06-13 16:36:55 -0400

    Trabalhando com Polices

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -13,7 +13,10 @@ class ProductsController extends Controller
     }
 
     public function store(Request $request){
-        return Product::create($request->all());
+        
+        $data = $request->all();
+        $data['user_id'] = \Auth::user()->id;
+        return Product::create($data);
     }
 
     public function update(Request $request, Product $product)
@@ -30,6 +33,7 @@ class ProductsController extends Controller
 
     public function destroy(Request $request, Product $product)
     {
+        $this->authorize('delete', $product);
         $product->delete();
         return $product;
     }
